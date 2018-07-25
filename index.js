64398259f7980bc206b3e75707d490078c8c9460
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-25 19:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop(drivers[-1])
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift(drivers[0])
+}
+
+function appendDriver(name){
+let newdrivers = drivers.push(name)
+ return newdrivers
+}
+
+appendDriver("Broom")
